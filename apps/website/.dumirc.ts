430ce96e96cc4a37e3e625523d9372b7fc125352
zxwin <zxwin_0125@163.com>
2025-06-10 19:37:10 +0800

    feat: :sparkles: add dumi-theme-antd-style

--- apps/website/.dumirc.ts
+++ apps/website/.dumirc.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'dumi'
+import type { SiteThemeConfig } from 'dumi-theme-antd-style';
 import path from 'node:path'
 
+const themeConfig: SiteThemeConfig = {
+  name: 'Ant Design Style Dumi Theme',
+}
+
 export default defineConfig({
-  outputPath: 'docs-dist',
-  npmClient: 'pnpm',
+  outputPath: 'docs-dist', // 输出路径
   resolve: {
     // 配置入口文件路径，API 解析将从这里开始
     entryFile: path.join(__dirname, '../../packages/ui/src/index.ts')
-  }
+  },
+  themeConfig
 })
