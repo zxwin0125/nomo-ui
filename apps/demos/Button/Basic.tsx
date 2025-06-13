a9986fb878465533f4a36850c2cd3ef7eec3af18
zxwin <zxwin_0125@163.com>
2025-06-13 12:01:43 +0800

    feat: :sparkles: add nomo-ui Button demo

---
+++ apps/demos/Button/Basic.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Button } from '@nomo/ui'
+
+const Basic: React.FC = () => {
+  return <Button>Click</Button>
+}
+
+export default Basic
---
+++ apps/demos/Button/Disabled.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Button } from '@nomo/ui'
+
+const Disabled: React.FC = () => {
+  return <Button>cannot click me</Button>
+}
+
+export default Disabled
